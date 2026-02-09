bfb7ce55e491fc486144186dc0e3f85739747c2f
ANABIA RAO <shaheerrao677@gmail.com>
2026-02-09 12:31:01 +0500

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.querySelector("#my-byt").addEventListener("click",function(){
+    document.body.style.backgroundColor = "pink"
+})
+document.querySelector("#blue").addEventListener("click",function(){
+    document.body.style.backgroundColor = "blue"
+})
+document.querySelector("#green").addEventListener("click",function(){
+    document.body.style.backgroundColor = "green"
+})
+document.querySelector("#yellow").addEventListener("click",function(){
+    document.body.style.backgroundColor = "yellow"
+})
+document.querySelector("#black").addEventListener("click",function(){
+    document.body.style.backgroundColor = "black"
+})
